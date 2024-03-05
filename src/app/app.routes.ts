599f813d73ba053edf0cd3897cfb26862ac0265d
HashanCharuk <charukhashan@gmail.com>
2024-03-05 10:46:53 +0530

    Corecction router links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './page/login/login.component';
+import { ViewAllBooksComponent } from './page/view-all-books/view-all-books.component';
+import { Component } from '@angular/core';
+import { RegisterComponent } from './page/register/register.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path:"login",
+        component: LoginComponent
+    },
+    {
+        path:"view-all-book",
+        component:ViewAllBooksComponent
+    },
+    {
+        path:"sign-up",
+        component:RegisterComponent
+
+    }
+];
