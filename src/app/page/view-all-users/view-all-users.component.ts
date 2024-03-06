18d968bce46470a1f507de4942c08793bf7ae2ed
HashanCharuk <charukhashan@gmail.com>
2024-03-06 22:19:41 +0530

    Completed user form

--- src/app/page/view-all-users/view-all-users.component.ts
+++ src/app/page/view-all-users/view-all-users.component.ts
@@ -2,17 +2,21 @@ import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
+import { NavComponent } from "../../common/nav/nav.component";
 
 @Component({
-  selector: 'app-view-all-users',
-  standalone: true,
-  imports: [HttpClientModule,FormsModule,CommonModule],
-  templateUrl: './view-all-users.component.html',
-  styleUrl: './view-all-users.component.css'
+    selector: 'app-view-all-users',
+    standalone: true,
+    templateUrl: './view-all-users.component.html',
+    styleUrl: './view-all-users.component.css',
+    imports: [HttpClientModule, FormsModule, CommonModule, NavComponent]
 })
 export class ViewAllUsersComponent implements OnInit {
 
   public userList:any;
+  private baseURL:String = "http://localhost:8081";
+
   public selectedUser:any={
     "id":null,
     "firstName":null,
@@ -30,12 +34,32 @@ export class ViewAllUsersComponent implements OnInit {
     this.loadUsers();
   }
   loadUsers(){
-    this.http.get("http://localhost:8081/user/get-all-users").subscribe((res:any)=>{
+    this.http.get(this.baseURL+"/user/get-all-users").subscribe((res:any)=>{
       console.log(res);
       this.userList=res;
     })
   }
   deleteUser(){
-
+    this.http.delete(this.baseURL+"/user/delete/"+this.selectedUser.id,{responseType:'text'}).subscribe((res:String)=>{   
+      this.loadUsers();
+      Swal.fire({
+        title:"Deleted",
+        text:`You Deleted${this.selectedUser.userName} success`,
+        icon: "success" 
+      })
+    })
+  }
+  setSelectedUser(user:any){
+    this.selectedUser=user;
+  }
+  saveUser(){
+    this.http.post(this.baseURL+"/user/addUser",this.selectedUser).subscribe((res:any)=>{
+      this.loadUsers();
+      Swal.fire({
+        title:"Updated !",
+        text:`You Updated ${this.selectedUser.userName} success`,
+        icon: "success" 
+      })
+    })
   }
 }
