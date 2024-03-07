7537fac09a3996efec68e7beb54d2e92beb37ef8
HashanCharuk <charukhashan@gmail.com>
2024-03-07 13:06:01 +0530

    Completed Login form

--- src/app/page/login/login.component.ts
+++ src/app/page/login/login.component.ts
@@ -1,12 +1,37 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,HttpClientModule,FormsModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  loginObj:any={
+    "email":"",
+    "password":""
+  }
+  constructor(private http:HttpClient,private router:Router){
+  }
+  login(){
+    this.http.post("http://localhost:8081/login/requestLogin",this.loginObj).subscribe((res:any)=>{
+      console.log(res);
+      if(res==true){
+        this.router.navigate(["/view-all-book"])
+      } else{
+        Swal.fire({
+          title:"Error",
+          text:`You can't Login..!`,
+          icon:"error"
+        })
+      }     
+    })
+  }
 }
+
